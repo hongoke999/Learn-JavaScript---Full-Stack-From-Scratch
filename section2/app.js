98bd520e83a0987d93afd89a2cfbb0191b72ff80
Chaka Hongoke <hongoke999@yahoo.com>
2020-06-23 15:00:40 +0300

    Scope and Context-part2

--- section2/app.js
+++ section2/app.js
@@ -1,17 +1,27 @@
 /* 
   Scope is the biggest source of confusion regarding variables
   Context is the biggest source of confusion regarding objects 
-*/
-let myName = 'Chaka';
 
-function amazingFunction() {
-  // let myName = 'Jino moja mswaki wa nini!!!';
-  if(2 + 2 === 4) {
-    myName = 'Vlad the impaler';
-    console.log("inside the if statement", myName);
+  The 'this' keyword points towards the object that is executing the current function
+*/
+let john = {
+  firstName: "John",
+  lastName: "Doe",
+  driveCar() {
+    function imAFunctionNotAMethod() {
+      console.log(this);
+    }
+    imAFunctionNotAMethod();
+    console.log(this.firstName + " " + this.lastName + " is driving a car.");
   }
-  // console.log("inside our function", myName);
 }
 
-amazingFunction();
-console.log("in the global scope", myName)
+john.driveCar();
+
+// Attaching independent function to an object to make it its method
+
+function breath() {
+  console.log(this.firstName + ' ' + this.lastName + ' just inhaled and exhaled.');
+}
+
+breath.call(john);
