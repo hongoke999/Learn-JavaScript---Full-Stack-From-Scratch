98cafbf7d085976ea59908eb985b43543b5b9857
Chaka Hongoke <hongoke999@yahoo.com>
2020-06-26 06:44:17 +0300

    Express Intro (Part 1)

--- section3/unicorn/test.js
+++ section3/unicorn/test.js
@@ -1,14 +1,2 @@
-let http = require('http');
+let express = require('express');
 
-let ourApp = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.end('Hello, and Welcome to our website.');
-    } 
-
-    if (req.url === '/about') {
-        res.end('Thank you for the interest in our company.');
-    }
-
-    res.end('We cannot find the page you are looking for.');
-});
-ourApp.listen(3000);
