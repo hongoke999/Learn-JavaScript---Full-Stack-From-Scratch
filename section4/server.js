a3334bf208b3368bbf8ca08fc2db59e4a9fc09ad
Chaka Hongoke <hongoke999@yahoo.com>
2020-06-27 10:01:36 +0300

    Connecting Node App to Database

--- section4/server.js
+++ section4/server.js
@@ -1,6 +1,15 @@
 let express = require("express");
+let mongodb = require("mongodb");
 
 let app = express();
+
+let db;
+
+let connectionString = 'mongodb+srv://todoAppUser:todoAppUser@cluster0-ijr3a.mongodb.net/TodoApp?retryWrites=true&w=majority'
+mongodb.connect(connectionString, {useNewUrlParser: true}, (err, client) => {
+    db = client.db();
+    app.listen(3000);
+});
 
 app.use(express.urlencoded({extended: false}))
 
@@ -58,8 +67,8 @@ app.get('/', (req, res) => {
 });
 
 app.post("/create-item", (req, res) => {
-    console.log(req.body.item);
-    res.send("Thanks for submitting the form.");
+    db.collection('items').insertOne({text: req.body.item}, () => {
+        res.send('Thanks for submitting the form.');
+    });    
 });
 
-app.listen(3000);
