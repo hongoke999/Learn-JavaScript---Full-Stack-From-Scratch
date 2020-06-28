9e5421312b64d53e90daae5eb109950e2b542c90
Chaka Hongoke <hongoke999@yahoo.com>
2020-06-28 12:50:16 +0300

    Create New Item Without Page Reload

--- section4/server.js
+++ section4/server.js
@@ -34,15 +34,15 @@ app.get('/', (req, res) => {
                 <h1 class="display-4 text-center py-1">To-Do App</h1>
                 
                 <div class="jumbotron p-3 shadow-sm">
-                <form action="/create-item" method="POST">
+                <form id="create-form" action="/create-item" method="POST">
                     <div class="d-flex align-items-center">
-                    <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+                    <input id="create-field" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
                     <button class="btn btn-primary">Add New Item</button>
                     </div>
                 </form>
                 </div> 
 
-                <ul class="list-group pb-5">
+                <ul id="item-list" class="list-group pb-5">
                     ${items.map((item) => {
                         return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
                           <span class="item-text">${item.text}</span>
@@ -50,7 +50,7 @@ app.get('/', (req, res) => {
                             <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">
                               Edit
                             </button>
-                            <button class="delete-me btn btn-danger btn-sm">
+                            <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">
                               Delete
                             </button>
                           </div>
@@ -67,8 +67,8 @@ app.get('/', (req, res) => {
 });
 
 app.post("/create-item", (req, res) => {
-    db.collection('items').insertOne({text: req.body.item}, () => {
-        res.redirect('/');
+    db.collection('items').insertOne({text: req.body.text}, (err, info) => {
+        res.json(info.ops[0])
     });    
 });
 
@@ -77,4 +77,10 @@ app.post('/update-item', function(req, res) {
     res.send("Success")
   })
 })
+
+app.post('/delete-item', (req, res) => {
+  db.collection('items').deleteOne({_id: new mongodb.ObjectId(req.body.id)}, () => {
+    res.send("Success");
+  });
+});
 
