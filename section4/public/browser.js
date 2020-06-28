d21180f1b8f464b5e8f8513b88c7fddd3db6952a
Chaka Hongoke <hongoke999@yahoo.com>
2020-06-28 16:56:46 +0300

    Client-Side Rendering

--- section4/public/browser.js
+++ section4/public/browser.js
@@ -8,6 +8,12 @@ function itemTemplate(item) {
   </li>`;
 }
 
+// Initial Page Load Render
+let ourHTML = items.map((item) => {
+    return itemTemplate(item)
+}).join('');                
+document.getElementById("item-list").insertAdjacentHTML("beforeend", ourHTML);
+
 // Create Feature
 let createField = document.getElementById('create-field');
 
